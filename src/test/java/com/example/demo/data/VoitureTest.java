9bc9f54f069f4cebff84274fcb1238b38fc53774
Luka-jev <unemailnormal23@gmail.com>
2026-05-20 11:33:03 +0200

    Add unit tests for Voiture class

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -10,8 +10,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VoitureTest {
 
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void testConstructeurEtGetters() {
+        Voiture v = new Voiture("Tesla", 50000);
+        assertEquals("Tesla", v.getMarque());
+        assertEquals(50000, v.getPrix());
+    }
+
+    @Test
+    void testSetters() {
+        Voiture v = new Voiture();
+        v.setMarque("Renault");
+        v.setPrix(20000);
+        v.setId(1);
+        
+        assertEquals("Renault", v.getMarque());
+        assertEquals(20000, v.getPrix());
+        assertEquals(1, v.getId());
+    }
+
+    @Test
+    void testToString() {
+        Voiture v = new Voiture("BMW", 30000);
+        v.setId(42);
+        String expected = "Car{marque='BMW', prix=30000, id=42}";
+        assertEquals(expected, v.toString());
     }
 
 }
